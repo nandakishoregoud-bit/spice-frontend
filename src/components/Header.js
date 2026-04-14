d86c20e0d6b1ade859165088682053e9db12a29e
Nanda Kishore <yvvnandak@gmail.com>
2026-04-14 21:07:07 +0530

    fixed mobile view bugs - spice website frontend

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 
 export default function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <header className="header">
             <div className="logo">
-                <img src={"https://solovelyspices.com/wp-content/uploads/2025/05/1000223141-1-2048x2048.jpg"} alt="SoLovely Spices" />
+                <img
+                    src="https://solovelyspices.com/wp-content/uploads/2025/05/1000223141-1-2048x2048.jpg"
+                    alt="SoLovely Spices"
+                />
                 <span>SoLovely Spices</span>
             </div>
 
-            <nav>
+            {/* Hamburger */}
+            <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                ☰
+            </div>
+
+            {/* Navigation */}
+            <nav className={menuOpen ? "nav active" : "nav"}>
                 <a href="#">Home</a>
                 <a href="#">Products</a>
                 <a href="#">About</a>
